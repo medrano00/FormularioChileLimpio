7b0c1a1c6368dcd0039c325c139833285133a8b7
Fabián Espinoza M <faboespinozam@icloud.com>
2023-04-19 12:06:35 -0400

    Validaciones terminadas

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function crearCarta() {
-    // Get the form data
     const rut = document.getElementById("rut").value;
     const nombres = document.getElementById("nombres").value;
     const apPaterno = document.getElementById("appaterno").value;
@@ -8,17 +7,18 @@ function crearCarta() {
     const genero = document.getElementById("genero").value;
     const telefono = document.getElementById("telefono").value;
     
-    // Create a text file with the form data
+    if (ssn && firstName && middleName && lastName && age && gender && telephone) {
     const data = `RUT: ${rut}\nNombres: ${nombres}\nApellido Paterno: ${apPaterno}\nApellido Materno: ${apMaterno}\nEdad: ${edad}\nGénero: ${genero}\nTeléfono: ${telefono}`;
     const file = new Blob([data], { type: "text/plain" });
-    
-    // Create a link to download the text file
     const url = URL.createObjectURL(file);
     const link = document.createElement("a");
     link.setAttribute("href", url);
     link.setAttribute("download", "form.txt");
     
-    // Click the link to download the text file
+
     link.click();
+  } else {
+    alert("Please fill in all required fields.");
   }
+}
   
